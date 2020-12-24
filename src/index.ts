dbd16f4f61f5e9769cda3f64007443a6f74163bf
Dmitriy Khovrich <dkhovrich@readdle.com>
2020-12-24 21:48:52 +0200

    Try to compose program IO

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import { getPostById } from "./api/posts";
 import { pipe } from "fp-ts/function";
 import * as TE from "fp-ts/TaskEither";
+import * as IO from "fp-ts/IO";
+import { randomInt } from "fp-ts/Random";
 
 const result = pipe(
   1,
   getPostById,
-  TE.map((post) => console.log("POST", post)),
+  TE.map((post) => {
+    // console.log("POST", post);
+    return post;
+  }),
   TE.mapLeft((error) => console.log("ERROR", error))
 );
 
 result().then();
+
+const log = (s: unknown): IO.IO<void> => () => console.log(s);
+
+const program: IO.IO<void> = pipe(
+  randomInt(1, 100),
+  IO.map(getPostById),
+  IO.chain(log)
+);
+
+program();
